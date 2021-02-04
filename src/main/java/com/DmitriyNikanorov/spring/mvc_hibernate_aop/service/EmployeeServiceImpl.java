d4cf2d22ac526d501001bdd6792dffa0982d7ba4
admi113 <nika1pb113@gmail.com>
2021-02-04 18:18:04 +0300

    Initial commit

---
+++ src/main/java/com/DmitriyNikanorov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.DmitriyNikanorov.spring.mvc_hibernate_aop.service;
+
+import com.DmitriyNikanorov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.DmitriyNikanorov.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    @Autowired
+    private EmployeeDAO employeeDao;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDao.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDao.saveEmployee(employee);
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/employee-info.jsp
@@ -0,0 +1,23 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+!OCTYPE html>
+<html>
+<body>
+
+<br>
+
+<form:form action="saveEmployee" modelAttribute="employee">
+    Name<form:input path="name"/>
+    <br>
+    Surname<form:input path="surname"/>
+    <br>
+    Department<form:input path="department"/>
+    <br>
+    Salary<form:input path="salary"/>
+    <br>
+    <input type="submit" value="OK">
+
+</form:form>
+
+
+</body>
+</html>
